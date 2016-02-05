4b59fc42873352a28e4396fad2f8f519fc3e05d7
thblt <thibault@thb.lt>
2016-02-05 12:29:13 +0100

    Fix Mac Azerty keybindings in XMonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -4,8 +4,10 @@ My XMonad configuration.
 Note this is basically a modified copy of the default config. 
 -}
 
-{-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE TemplateHaskell #-} 
 
+import Language.Haskell.TH
+import System.Posix.Unistd (getSystemID, SystemID (nodeName) )
 import XMonad
 import XMonad.Actions.WindowBringer
 import XMonad.Hooks.DynamicLog
@@ -19,16 +21,28 @@ import XMonad.Layout.ThreeColumns
 import XMonad.Layout.ToggleLayouts (toggleLayouts, ToggleLayout (ToggleLayout, Toggle))
 import qualified XMonad.StackSet as XSS
 import XMonad.Util.Run (spawnPipe)
-
+import XMonad.Util.Scratchpad 
 import Graphics.X11.ExtraTypes.XF86
-
 import qualified Data.Map as M
 import qualified Data.List as L
 import System.Exit (exitSuccess)
-import System.IO
+import System.IO (hPutStrLn)
+
+[d| myHostName = $(stringE =<< runIO (fmap nodeName getSystemID) ) |]
 
 myWorkspaces :: [ String ]
 myWorkspaces = map show [ 1 .. 9 :: Int ]
+
+workspacesKeys :: [KeySym]
+workspacesKeys | myHostName == "anna" = macAzertyKeys
+               | otherwise = pcAzertyKeys
+  where
+    pcAzertyKeys = [0x26,0xe9,0x22,0x27,0x28,0x2d,0xe8,0x5f,0xe7,0xe0] -- From AzertyConfig
+    macAzertyKeys = [0x26,0xe9,0x22,0x27,0x28,0xa7,0xe8,0x21,0xe7,0xe0] -- From AzertyConfig
+        
+
+hiddenWorkspaces :: [String]
+hiddenWorkspaces = [ "NSP" ]
 
 myLayout = avoidStruts . smartBorders $ toggleLayouts Full $
   tall
@@ -38,8 +52,7 @@ myLayout = avoidStruts . smartBorders $ toggleLayouts Full $
   ||| spiral (6/7)
     where tall = Tall 1 (3/100) (1/2)
 
-layoutNames :: [String]
--- This is used by the pretty printer.
+layoutNames :: [String] -- This is used by the pretty printer.
 layoutNames = [
     "Tall"
   , "ThreeCol"
@@ -52,10 +65,21 @@ layoutNames = [
 myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
 myKeys conf@XConfig {XMonad.modMask = modMask} = M.fromList $
   [
-  -- Fundamentals
+    -- Fundamentals
     ((modMask .|. shiftMask, xK_c), kill) -- Kill focused window
+  , ((modMask .|. shiftMask, xK_q     ), io exitSuccess) -- %! Quit xmonad
+  , ((modMask, xK_q     ), spawn $ unlines [
+          "xmonad --recompile"
+        , "if [ $? -eq 0 ]; then"
+        , "    xmonad --restart"
+        , "    notify-send -u low XMonad 'Recompiled and restarted.'"
+        , "else"
+        , "    notify-send -u critical \"XMonad recompilation failed\" \"\n$(cat ~/.xmonad/xmonad.errors)\""
+        , "fi"
+        ]
+     )
 
-  -- Layout transitions
+    -- Layout transitions
   , ((modMask, xK_space), sendMessage NextLayout) -- Next layout
   , ((modMask .|. shiftMask, xK_space), setLayout $ XMonad.layoutHook conf) -- Reset layouts
   , ((modMask, xK_h ), sendMessage Shrink) -- %! Shrink the master area
@@ -63,30 +87,28 @@ myKeys conf@XConfig {XMonad.modMask = modMask} = M.fromList $
   , ((modMask, xK_f ), sendMessage ToggleStruts)
   , ((modMask .|. shiftMask, xK_f ), sendMessage ToggleLayout)
    
-  -- increase or decrease number of windows in the master area
+    -- increase or decrease number of windows in the master area
   , ((modMask              , xK_comma ), sendMessage (IncMasterN 1)) -- %! Increment the number of windows in the master area
   , ((modMask              , xK_semicolon), sendMessage (IncMasterN (-1))) -- %! Deincrement the number of windows in the master area
     
-  -- Move between windows
+    -- Move between windows
   , ((modMask, xK_Tab ), windows XSS.focusDown) 
   , ((modMask .|. shiftMask, xK_Tab ), windows XSS.focusUp )
   , ((modMask, xK_m ), windows XSS.focusMaster )
   , ((modMask, xK_Return), windows XSS.swapMaster)
   , ((modMask, xK_j ), windows XSS.swapDown )
   , ((modMask, xK_k ), windows XSS.swapUp )
-  -- floating layer support
+    
+    -- floating layer support
   , ((modMask, xK_t     ), withFocused $ windows . XSS.sink) -- %! Push window back into tiling
     
-    -- quit, or restart
-  , ((modMask .|. shiftMask, xK_q     ), io exitSuccess) -- %! Quit xmonad
-  , ((modMask, xK_q     ), spawn "xmonad --recompile && xmonad --restart; notify-send -i ~/.xmonad/notifIcon.png -u low XMonad 'Recompiled and restarted.'") -- %! Restart xmonad
-    
-  -- Launchers
+    -- Launchers
   , ((modMask, xK_p ), spawn "synapse" )
   , ((modMask .|. shiftMask, xK_p), spawn "dmenu_run" )
-  , ((modMask .|. shiftMask, xK_Return ), spawn "urxvt" )
+  , ((modMask .|. shiftMask, xK_Return ), spawn $ terminal conf)
+  , ((modMask, xK_s ), scratchpadSpawnActionTerminal $ terminal conf)
 
-  -- Bring/Goto
+    -- Bring/Goto
   , ((modMask, xK_g), gotoMenu)
   , ((modMask .|. shiftMask, xK_g), bringMenu)
     
@@ -99,7 +121,7 @@ myKeys conf@XConfig {XMonad.modMask = modMask} = M.fromList $
   ]
   ++
   [((m .|. modMask, k), windows $ f i)
-        | (i, k) <- zip (XMonad.workspaces conf) [0x26,0xe9,0x22,0x27,0x28,0x2d,0xe8,0x5f,0xe7,0xe0]
+        | (i, k) <- zip (XMonad.workspaces conf) workspacesKeys
         , (f, m) <- [(XSS.greedyView, 0), (XSS.shift, shiftMask)]]
   ++
   -- mod-{a,z,e} %! Switch to physical/Xinerama screens 1, 2, or 3
@@ -109,8 +131,8 @@ myKeys conf@XConfig {XMonad.modMask = modMask} = M.fromList $
         , (f, m) <- [(XSS.view, 0), (XSS.shift, shiftMask)]]
 
 {-
-XMobar configuration.  - For consistency, xmobar isn't configured
-through an external file, but with command-line parameters. This
+XMobar configuration.  - For consistency, xmobar isn't fully configured
+through an external file, but also with command-line parameters. This
 allows to keep things such as color scheme in a single place, and I
 probably won't be using Xmobar outside of Xmonad.
 -}
@@ -162,20 +184,18 @@ pp_surround a b = a ++ b ++ a
 prettyPrinter :: PP
 prettyPrinter = def
   {
-  {-  ppHidden = \w -> xmobarColor "ff0000" "ffffff" . icon $ "SVGtest/" ++ w ++ "_empty.xpm"
-  , ppCurrent = \w -> "<fc=#000000> <icon=SVGtest/" ++ w ++ "_full.xpm/></fc>"
-  , ppHiddenNoWindows = \w -> "<fc=#00eeee> <icon=SVGtest/" ++ w ++ "_empty.xpm/></fc>" -}
-    ppCurrent = \w -> pp_active $ pp_font 1 w
-  , ppHidden = \w -> pp_inactive w
-  , ppHiddenNoWindows = \w -> pp_disabled w
+    ppCurrent = \w -> handleHiddenWS w $ pp_active . pp_font 1
+  , ppHidden = \w -> handleHiddenWS w $ pp_inactive
+  , ppHiddenNoWindows = \w -> handleHiddenWS w $ pp_disabled 
   , ppTitle = pp_default . pp_unsafe 
   , ppSep = "        "
-  , ppLayout = layoutPrinter
+  , ppLayout = \a -> concat $ L.intersperse " " $ fmap (makeIcon a) layoutNames
   }
   where
-    layoutPrinter c = concat $ L.intersperse " " $ fmap makeIcon layoutNames
-      where makeIcon l   | c==l = pp_active $ pp_icon $ "layout_" ++ l
-                         | otherwise = pp_inactive $ pp_icon $ "layout_" ++ l
+    handleHiddenWS w f | w `elem` hiddenWorkspaces = ""
+                       | otherwise = f w 
+    makeIcon a l | a==l = pp_active $ pp_icon $ "layout_" ++ l
+                       | otherwise = pp_inactive $ pp_icon $ "layout_" ++ l
 
 {- And now to wrap it all up -}
 
@@ -188,15 +208,18 @@ main = do
                                                  ]
   xmonad def
     {
-      startupHook = setWMName "LG3D",
-      modMask = mod4Mask -- ``Windows'' key.
+      startupHook = setWMName "LG3D"
+    , terminal = "urxvt"
+    , modMask = mod4Mask -- ``Windows'' key.
     , workspaces = myWorkspaces
     , keys = myKeys
     , layoutHook = myLayout
     , handleEventHook = docksEventHook
-    , manageHook = composeAll
+    , manageHook = composeAll 
       [
         className =? "Gloobus-preview" --> doFloat
+--      , className =? "scratchpad" --> doFloat
+      , scratchpadManageHook $ XSS.RationalRect 0.1 0.1 0.8 0.8
       ]
     , focusFollowsMouse = False
     , clickJustFocuses = False
