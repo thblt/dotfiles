f16444d440912d026f806ce3c85baf4e28d9d3c5
thblt <thibault@thb.lt>
2016-08-06 16:08:08 +0200

    Change Spaceline LAF

--- .emacs.d/user-lisp/setup-spaceline.el
+++ .emacs.d/user-lisp/setup-spaceline.el
@@ -2,7 +2,9 @@
   :ensure spaceline
   :config
   (spaceline-spacemacs-theme)
-  (setq spaceline-highlight-face-func 'spaceline-highlight-face-evil-state)
+  (setq spaceline-highlight-face-func 'spaceline-highlight-face-evil-state
+        powerline-default-separator 'alternate)
   )
+
 
 (provide 'setup-spaceline)
