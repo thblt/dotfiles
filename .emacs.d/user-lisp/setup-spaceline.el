b543c231a4511f6c8f91b6c691f94ff285bd8160
thblt <thibault@thb.lt>
2016-08-22 14:16:20 +0200

    Fix spaceline

--- .emacs.d/user-lisp/setup-spaceline.el
+++ .emacs.d/user-lisp/setup-spaceline.el
@@ -1,9 +1,9 @@
 (use-package spaceline-config
   :ensure spaceline
   :config
-  (spaceline-spacemacs-theme)
   (setq spaceline-highlight-face-func 'spaceline-highlight-face-evil-state
         powerline-default-separator 'alternate)
+  (spaceline-spacemacs-theme)
   )
 
 
