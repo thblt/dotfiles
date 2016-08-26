19593bdf201aa1873d1d32f736f5658becb916de
thblt <thibault@thb.lt>
2016-08-27 01:39:55 +0200

    Generally improve setup-editing.el

--- .emacs.d/user-lisp/setup-editing.el
+++ .emacs.d/user-lisp/setup-editing.el
@@ -1,15 +1,8 @@
 (require 'use-package)
 
-(use-package nlinum ; More efficient line numbering, especially on large files with huge foldings (eg org)
-  :config (nlinum-mode)
-  )
-(use-package linum-relative             ; Relative line numbers
-;;  :init (linum-relative-global-mode)
-  :config (setq linum-relative-current-symbol ""
-                linum-relative-with-helm nil)
-  )
 
-;; Editing
+
+(use-package aggressive-indent)
 (use-package anzu)                      ; Show matches count/current match # in mode line
 (use-package avy                        ; Jump, move and copy everywhere (similar to Vim-EasyMotion)
   :bind (("C-:" . avy-goto-char-timer)
@@ -17,25 +10,18 @@
          ("C-=" . avy-goto-line)
          )
   )
-(use-package unfill
-  :bind (
-         ("M-Q" . unfill-paragraph)
-         )
-  )
-
-(use-package undo-tree
-	     :config (diminish 'undo-tree-mode)
-	     )
 (use-package expand-region)             ; Expand region by semantic units
 (use-package highlight-indentation)     ; Show indent level markers
-(use-package rainbow-delimiters)        ; Colorize parentheses etc by depth.
-(use-package smartparens-config          ; Be smart with parentheses
-  :ensure smartparens
-  :init (progn
-          (show-smartparens-global-mode t)
-          )
+(use-package linum-relative             ; Relative line numbers
+  :init (add-hook 'prog-mode-hook
+                  'linum-relative-mode t)
+  :config (setq linum-relative-current-symbol ""
+                linum-relative-with-helm nil)
   )
-(use-package aggressive-indent)
+(use-package nlinum ; More efficient line numbering, especially on large files with huge foldings (eg org)
+  :config (nlinum-mode)
+  )
+(use-package rainbow-delimiters)        ; Colorize parentheses etc by depth.
 (use-package smart-tabs-mode
 ;;  :init (add-hook 'c-mode-common-hook (lambda ()
 ;;                                    (smart-tabs-mode-enable)
@@ -43,7 +29,20 @@
 ;;                  )
 ;;  :config (smart-tabs-insinuate 'c 'c++ 'python 'javascript)
   )
-
+(use-package smartparens-config         ; Be smart with parentheses
+  :ensure smartparens
+  :init (progn
+          (smartparens-global-mode t)
+          )
+  )
+(use-package undo-tree
+	     :config (diminish 'undo-tree-mode)
+	     )
+(use-package unfill                      ; Unfill
+  :bind (
+         ("M-Q" . unfill-paragraph)
+         )
+  )
 (use-package writeroom-mode)            ; Distraction-free mode
 (use-package yasnippet                  ; Snippets
   :init (yas-global-mode)
