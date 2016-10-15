ebb4a79b38e8da0e4d44f7fc774b6bece4c21f04
thblt <thibault@thb.lt>
2016-10-15 16:26:13 +0200

    Use skewer

--- .emacs.d/user-lisp/setup-webdev.el
+++ .emacs.d/user-lisp/setup-webdev.el
@@ -1,11 +1,8 @@
-(use-package scss-mode)                 ; (S)CSS
-
-(use-package less-css-mode)             ; LESS
-
-(use-package haml-mode)                 ; HAML templates
-
-(use-package web-mode)                  ; HTML and HTML templates
-
 (use-package emmet-mode)                ; "Zen" XML
+(use-package haml-mode)                 ; HAML templates
+(use-package less-css-mode)             ; LESS
+(use-package scss-mode)                 ; (S)CSS
+(use-package skewer-mode)               ; JSFiddle/CodePen like mode
+(use-package web-mode)                  ; HTML and HTML templates
 
 (provide 'setup-webdev)
