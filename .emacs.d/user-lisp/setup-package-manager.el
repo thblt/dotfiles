f2ba45d494aafc1c7711d27a919ae812ff5aba85
thblt <thibault@thb.lt>
2016-10-15 16:25:53 +0200

    Don't use marmalade

--- .emacs.d/user-lisp/setup-package-manager.el
+++ .emacs.d/user-lisp/setup-package-manager.el
@@ -1,8 +1,7 @@
 (require 'package)
 
-(setq package-archives '(("gnu"       . "https://elpa.gnu.org/packages/")
-                         ("marmalade" . "https://marmalade-repo.org/packages/")
-                         ("melpa"     . "https://melpa.org/packages/")))
+(setq package-archives '(("gnu"   . "https://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
 
 (package-initialize)
 
