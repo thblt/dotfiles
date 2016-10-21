bf6a903a870562508620726f878242ed8348e4b2
thblt <thibault@thb.lt>
2016-10-21 17:24:32 +0200

    Ignore suffixes instead of globs in projectile

--- .emacs.d/user-lisp/setup-projectile.el
+++ .emacs.d/user-lisp/setup-projectile.el
@@ -1,11 +1,12 @@
 ;; Project management
 (use-package projectile                 ; Project management
   :config (setq
-           projectile-globally-ignored-files (append '(
-                                                       "*.~undo-tree~"
-                                                       )
-                                                     projectile-globally-ignored-files
-                                                     )
+           projectile-globally-ignored-file-suffixes (append '(
+                                                               ".un~"
+                                                               ".~undo-tree~"
+                                                               )
+                                                             projectile-globally-ignored-files
+                                                             )
            )
   :init (projectile-global-mode)
   )
