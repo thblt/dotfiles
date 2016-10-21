d4886713696ef292bded0bf1b9f4997a4b6e6bcc
thblt <thibault@thb.lt>
2016-10-21 13:08:18 +0200

    Fix git-timemachine evil bindings

--- .emacs.d/user-lisp/setup-git.el
+++ .emacs.d/user-lisp/setup-git.el
@@ -1,9 +1,12 @@
 ;; Versioning and history
 (use-package git-timemachine
-  :config   (progn
-              (evil-make-overriding-map git-timemachine-mode-map 'normal)
-              ;; force update evil keymaps after git-timemachine-mode loaded
-              (add-hook 'git-timemachine-mode-hook #'evil-normalize-keymaps) ) )
+  :config (eval-after-load "evil"
+            '(progn
+               (evil-make-overriding-map git-timemachine-mode-map 'normal)
+               ;; force update evil keymaps after git-timemachine-mode loaded
+               (add-hook 'git-timemachine-mode-hook #'evil-normalize-keymaps) )
+            )
+  )
 
 (use-package magit
   :bind ( ("C-x g" . magit-status) )
