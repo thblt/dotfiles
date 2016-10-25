ec1f06e460a63684a33e1dde8dad789ca73f7846
thblt <thibault@thb.lt>
2016-10-25 19:35:44 +0200

    Fix wording in setup-package-manager.el

--- .emacs.d/user-lisp/setup-package-manager.el
+++ .emacs.d/user-lisp/setup-package-manager.el
@@ -2,17 +2,17 @@
 
 (setq package-archives '(("gnu"          . "https://elpa.gnu.org/packages/")
                          ("melpa"        . "https://melpa.org/packages/")
-                         ("melpa-stable" . "https://stable.melpa.org/packages/")                         
+                         ("melpa-stable" . "https://stable.melpa.org/packages/")
                          )
 
       package-archive-priorities '(
                                    ("melpa-stable" . 20)
-                                   ("gnu"          . 15) 
+                                   ("gnu"          . 15)
                                    ("melpa"        . 10)
                                    ))
-      ;; TODO This is an experiment: I'm not sure putting above melpa
-      ;; unstable is a good idea: I don't know if GNU packages are
-      ;; recent enough.  We'll see if something breaks.
+      ;; TODO This is an experiment: I'm not sure putting GNU above
+      ;; Melpa unstable is a good idea: I don't know if GNU packages
+      ;; are recent enough.  We'll see if something breaks.
 
 (package-initialize)
 
