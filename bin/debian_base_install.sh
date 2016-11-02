e54448b9c31083176619197b44b86994716a124a
Thibault <thblt@anna>
2016-11-02 10:26:33 +0100

    Add Debian post-install script

---
+++ bin/debian_base_install.sh
@@ -0,0 +1,39 @@
+sudo apt install \
+	dunst \
+	chromium chromium-l10n \
+	compton \
+	dbus-x11 \
+	emacs \
+	feh \
+	git \
+	firefox-esr firefox-esr-l10n-fr \
+    isync \
+	keychain \
+	keynav \
+	libnotify-bin \
+	libghc-dbus-dev \
+	libghc-xmonad-contrib-doc \
+	light-locker \
+	lightdm \
+	lightdm-gtk-greeter \
+	lynx \
+	maildir-utils \
+	mu4e \
+	mupdf \
+    network-manager \
+	rxvt-unicode \
+	sudo \
+	synapse \
+	texlive \
+	tmux \
+	vim \
+	vim-gtk \
+    wipe \
+	wmctrl \
+	xmonad \
+	zsh \
+
+
+cd $HOME
+    
+git config --global core.excludesfile ~/.gitignore_global
