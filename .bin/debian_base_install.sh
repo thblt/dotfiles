b0db43bf243712e23fda32d64cb9889531dcf838
thblt <thibault@thb.lt>
2016-11-03 19:07:21 +0100

    Add some more packages in Debian base

--- .bin/debian_base_install.sh
+++ .bin/debian_base_install.sh
@@ -1,11 +1,13 @@
 apt install \
 	alsa-base alsa-utils \
-	build-essential git \
+	build-essential git python-setuptools python-pip python python3 virtualenvwrapper nodejs npm \
 	chromium chromium-l10n firefox-esr firefox-esr-l10n-fr lynx \
+    libreoffice vlc \
 	cups \
+    mc \
 	emacs vim vim-gtk \
 	firmware-linux-free firmware-linux-nonfree smartmontools \
-	font-manager \
+	font-manager fonts-roboto \
 	keychain \
 	keynav \
 	libnotify-bin \
