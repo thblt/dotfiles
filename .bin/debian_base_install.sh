dd998ed9a1b458a7abdec9fa4b512034262c2738
thblt <thibault@thb.lt>
2016-11-03 19:35:37 +0100

    Add packages and good advice to myself in debian postinstall script

--- .bin/debian_base_install.sh
+++ .bin/debian_base_install.sh
@@ -1,5 +1,6 @@
 apt install \
 	alsa-base alsa-utils \
+    apt-file \
 	build-essential git python-setuptools python-pip python python3 virtualenvwrapper nodejs npm \
 	chromium chromium-l10n firefox-esr firefox-esr-l10n-fr lynx \
     libreoffice vlc \
@@ -24,3 +25,7 @@ apt install \
 	wmctrl \
 	xmonad libghc-xmonad-contrib-doc libghc-dbus-dev dunst compton synapse feh dbus-x11 \
 	zsh \
+
+echo "Advice on how to install some extra packages in the file"
+
+# qt: qt5-default fournit qt et le configure comme installation par défaut pour qtchooser.
