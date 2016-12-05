1e10e1e039cf1ad144e7da32bf96fc024188b62e
thblt <thibault@thb.lt>
2016-12-05 14:10:46 +0100

    Fix eval-after-load

--- .emacs.d/user-lisp/setup-org.el
+++ .emacs.d/user-lisp/setup-org.el
@@ -4,19 +4,19 @@
   :init (add-hook 'org-mode-hook 'toc-org-enable)
   )
 
-(eval-after-load "org"
+(eval-after-load "org-mode"
   (progn
     (setq org-catch-invisible-edits t ; Avoid editing folded contents
           org-hide-leading-stars t
           org-hide-emphasis-markers t
           org-src-fontify-natively t  ; Syntax highlighting in src blocks. Kind of a lighter MMM.
           )
-;;    (add-hook 'org-mode-hook (lambda ()
-;;                               (flyspell-mode t)
-;;                               (org-indent-mode t)
-;;                               (visual-line-mode t)
-;;                               )
-;;              )
+    (add-hook 'org-mode-hook (lambda ()
+                               (flyspell-mode t)
+                               (org-indent-mode t)
+                               (visual-line-mode t)
+                               )
+              )
     )
   )
 
