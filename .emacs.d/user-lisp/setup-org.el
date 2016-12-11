602035421ba52544a0ba2666e0ec9c1665caf7cf
thblt <thibault@thb.lt>
2016-12-11 19:01:59 +0100

    Cleanup

--- .emacs.d/user-lisp/setup-org.el
+++ .emacs.d/user-lisp/setup-org.el
@@ -1,21 +1,21 @@
 ;;; Code:
 
 (use-package org
-	     :ensure nil
-	     :init  (progn
-		      (setq org-catch-invisible-edits t ; Avoid editing folded contents
-			    org-hide-leading-stars t
-			    org-hide-emphasis-markers t
-			    org-src-fontify-natively t  ; Syntax highlighting in src blocks. Kind of a lighter MMM.
-			    )
-		      (add-hook 'org-mode-hook (lambda ()
-						 (flyspell-mode t)
-						 (org-indent-mode t)
-						 (visual-line-mode t)
-						 )
-				)
-		      )
-	     )
+  :ensure nil
+  :init  (progn
+           (setq org-catch-invisible-edits t ; Avoid editing folded contents
+                 org-hide-leading-stars t
+                 org-hide-emphasis-markers t
+                 org-src-fontify-natively t  ; Syntax highlighting in src blocks. Kind of a lighter MMM.
+                 )
+           (add-hook 'org-mode-hook (lambda ()
+                                      (flyspell-mode t)
+                                      (org-indent-mode t)
+                                      (visual-line-mode t)
+                                      )
+                     )
+           )
+  )
 
 (use-package ox-reveal)
 
