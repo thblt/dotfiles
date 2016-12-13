936f687298f2eb3bbe69604bb94e812ff9f9c83f
thblt <thibault@thb.lt>
2016-12-13 19:27:11 +0100

    Use evil-nerd-commenter

--- .emacs.d/user-lisp/setup-programming.el
+++ .emacs.d/user-lisp/setup-programming.el
@@ -1,10 +1,16 @@
-
 (setq compile-command "wmake" ; A small script which invokes the first
                               ; build system it can find instructions
                               ; for (in .dotfiles/bin)
       )
 
 (define-key prog-mode-map (kbd "<f8>") 'ffap)
+
+(use-package evil-nerd-commenter
+  :bind (("M-;"   . evilnc-comment-or-uncomment-lines)
+         ("C-M-;" . evilnc-comment-or-uncomment-paragraphs)         
+         ("C-c l" . evilnc-quick-comment-or-uncomment-to-the-line)
+         ("C-c c" . evilnc-copy-and-comment-lines)
+         ("C-c p" . evilnc-comment-or-uncomment-paragraphs)))
 
 (use-package company                    ; Completion framework
   :init (add-hook 'prog-mode-hook 'company-mode)
