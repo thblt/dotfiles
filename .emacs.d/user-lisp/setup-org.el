0452477ad7dee5a603f01f4b786285b1745b08de
thblt <thibault@thb.lt>
2016-12-13 17:45:56 +0100

    Use no modeline plugin

--- .emacs.d/user-lisp/setup-org.el
+++ .emacs.d/user-lisp/setup-org.el
@@ -2,19 +2,19 @@
 
 (use-package org
   :pin org
-  :init  (progn
-           (setq org-catch-invisible-edits t ; Avoid editing folded contents
-                 org-hide-leading-stars t
-                 org-hide-emphasis-markers t
-                 org-src-fontify-natively t  ; Syntax highlighting in src blocks. Kind of a lighter MMM.
-                 )
-           (add-hook 'org-mode-hook (lambda ()
-                                      (flyspell-mode t)
-                                      (org-indent-mode t)
-                                      (visual-line-mode t)
-                                      ))))
+  :init (progn
+          (setq org-catch-invisible-edits t ; Avoid editing folded contents
+                org-hide-leading-stars t
+                org-hide-emphasis-markers t
+                org-src-fontify-natively t  ; Syntax highlighting in src blocks. 
+                )
+          (add-hook 'org-mode-hook (lambda ()
+                                     (flyspell-mode t)
+                                     (org-indent-mode t)
+                                     (visual-line-mode t)
+                                     ))))
 
-; (use-package ox-reveal)
+(use-package ox-reveal)
 
 (use-package toc-org
   :init (add-hook 'org-mode-hook 'toc-org-enable))
