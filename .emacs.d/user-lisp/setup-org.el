7b1336291e7a56f04d91e9c887daae5919d79c8a
thblt <thibault@thb.lt>
2016-12-13 19:27:52 +0100

    Update org config

--- .emacs.d/user-lisp/setup-org.el
+++ .emacs.d/user-lisp/setup-org.el
@@ -6,13 +6,15 @@
           (setq org-catch-invisible-edits t ; Avoid editing folded contents
                 org-hide-leading-stars t
                 org-hide-emphasis-markers t
-                org-src-fontify-natively t  ; Syntax highlighting in src blocks. 
+                org-src-fontify-natively t  ; Syntax highlighting in src blocks.
                 )
           (add-hook 'org-mode-hook (lambda ()
                                      (flyspell-mode t)
                                      (org-indent-mode t)
                                      (visual-line-mode t)
-                                     ))))
+                                     )))
+  :diminish org-indent-mode)
+
 
 (use-package ox-reveal)
 
