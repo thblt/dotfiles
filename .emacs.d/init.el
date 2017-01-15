aae97d13d0ef2aa47482c8f2a589de544df862ee
thblt <thibault@thb.lt>
2017-01-15 14:14:53 +0100

    Defer everything

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -29,8 +29,10 @@
 (eval-and-compile
   (package-install 'use-package))
 
-(setq use-package-always-ensure t
+(setq use-package-always-defer t
+      use-package-always-ensure t
       use-package-verbose t)
+
 (require 'use-package)
 
 ;; We need to install org /before/ tangling dotemacs.org, or it will never update.
