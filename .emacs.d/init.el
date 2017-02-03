480f9b682d4f01d89c05ad814173d59216def036
thblt <thibault@thb.lt>
2017-02-03 21:14:32 +0100

    Remove useless comment

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -28,7 +28,3 @@
 
 (require 'org)
 (org-babel-load-file (expand-file-name "dotemacs.org" user-emacs-directory))
-
-;; Package.el wants this:
-;; (package-initialize)
-;; to be present in this file.
