4127957cd2715ce6867acf74366dfed95ba015a8
thblt <thibault@thb.lt>
2017-02-03 21:11:16 +0100

    Load org from gnu rather than Org Elpa

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,26 +1,25 @@
 (setq package-archives '(("gnu"          . "https://elpa.gnu.org/packages/")
                          ("melpa"        . "https://melpa.org/packages/")
-                         ("melpa-stable" . "https://stable.melpa.org/packages/")
-                         ("org" . "http://orgmode.org/elpa/"))
+                         ("melpa-stable" . "https://stable.melpa.org/packages/"))
+;;                         ("org" . "http://orgmode.org/elpa/"))
 
       ;; Emacs25+ only.  TODO This is an experiment: I'm not sure
       ;; putting GNU above Melpa unstable is a good idea: I don't know
       ;; if GNU packages are recent enough.  We'll see if something
       ;; breaks.
       package-archive-priorities '(
-                                   ("org"          . 1001)
                                    ("melpa-stable" . 1000)
                                    ("gnu"          . 500)
                                    ("melpa"        . 100))
-
-      package-enable-at-startup t
+;;                                   ("org"          . 1))
+				
+      package-enable-at-startup nil
 
       ;; Isolate package directories for major Emacs version
       package-user-dir (concat user-emacs-directory "/elpa" (number-to-string emacs-major-version))
       )
 
 (require 'package)
-
 (package-initialize)
 
 ;; We need to configure and (require) package.el _before_ requiring
