caf69f4daf498a13cdbb181053aae05817d36592
thblt <thibault@thb.lt>
2017-03-20 11:23:15 +0100

    Remove obsolete comment

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,10 +2,6 @@
                          ("melpa"        . "https://melpa.org/packages/")
                          ("melpa-stable" . "https://stable.melpa.org/packages/"))
 
-      ;; Emacs25+ only.  TODO This is an experiment: I'm not sure
-      ;; putting GNU above Melpa unstable is a good idea: I don't know
-      ;; if GNU packages are recent enough.  We'll see if something
-      ;; breaks.
       package-archive-priorities '(
                                    ("melpa-stable" . 1000)
                                    ("gnu"          . 500)
