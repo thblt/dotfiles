f1670830f1251de09b3dd3544111a304e0295c76
Thibault Polge <thibault@thb.lt>
2018-04-05 10:39:19 +0200

    New config.nix

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -1,69 +1,116 @@
-{
+    {
   allowUnfree = true;
 
-  packageOverrides = pkgs_: with pkgs_; {
-
-    emacs = stdenv.lib.overrideDerivation emacs (oldAttrs : {
-        src = fetchurl {
-          url = "mirror://gnu/emacs/emacs-25.2.tar.xz";
-          sha256 = "59b55194c9979987c5e9f1a1a4ab5406714e80ffcfd415cc6b9222413bc073fa";
-        };
-        patches = [];
-      });
-
-    all = with pkgs; buildEnv {
-      name = "all";
-      paths = [
-               zsh
-               tmux
-
-               tree
-
-               emacs
-               isync
-               mu
-
-               gnupg
-               gpa
-               pass
-               pinentry
-
-
-
-               arandr
-               fzf
-               ripgrep
-               git
-               meld
-
-               dmenu
-               dunst
-               libnotify               xsel
-
-               zip
-               unzip
-               p7zip
-               bc
-               firefoxWrapper
-               chromium
-               python27
-               python3
-               gcc
-               libreoffice
-               vlc
-               rxvt_unicode-with-plugins
-               wmctrl
-
-	       feh
-
-	       compton
-
-               krita
-               gimp
-               blender
-
-			stack
-               ];
-    };
+  chromium = {
+    enablePepperFlash = true;
   };
-}
+
+      packageOverrides = pkgs_: with pkgs_; {
+
+        # * Packages overrides
+          # ** Emacs 25
+
+          emacs = stdenv.lib.overrideDerivation emacs (oldAttrs : {
+              src = fetchurl {
+                url = "mirror://gnu/emacs/emacs-25.3.tar.xz";
+                sha256 = "253ac5e7075e594549b83fd9ec116a9dc37294d415e2f21f8ee109829307c00b";
+              };
+              patches = [];
+            });
+
+        # * Package list
+
+          all = with pkgs; buildEnv {
+          name = "all";
+          paths =
+            [
+
+             # ** Shell
+
+             tmux
+
+             # ** Common system utilities
+
+             zip
+             unzip
+             wget
+             whois
+             tree
+             p7zip
+
+             # ** Less common system utilities
+
+             bc
+             graphviz
+             pandoc
+             udiskie
+
+             # ** Crypto
+
+             gnupg1compat
+             gpa
+             pass
+             pinentry
+
+             # ** X11 and X utilities
+
+             arandr
+             compton
+             dmenu
+             dunst
+             feh
+             libnotify
+             lightlocker
+             powerline-fonts
+             scrot
+             wmctrl
+             xorg.xev
+             xsel
+
+             # *** Apps
+
+             chromium
+             latest.firefox-bin
+             browserpass
+             libreoffice
+             rxvt_unicode-with-plugins
+             vlc
+
+             # *** Fonts
+
+             opensans-ttf
+             roboto
+             symbola
+
+             # *** Icon/cursor themes
+
+             gnome3.adwaita-icon-theme # For large mouse pointers
+
+             # ** Emacs and friends
+
+             emacs
+             isync
+             mu
+             aspell
+             aspellDicts.fr
+             aspellDicts.en
+
+             hunspell
+             hunspellDicts.fr-any
+
+             # ** Programming tools
+
+             ripgrep
+             git
+             meld
+
+             # ** *TeX
+
+             asymptote
+             lyx
+             texlive.biber
+             texlive.combined.scheme-full
+             ];
+        };
+      };
+    }
