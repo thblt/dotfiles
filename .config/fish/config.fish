8c030ac309642123dc0a22ef30e6cd6b2bdbfde3
Thibault Polge <thibault@thb.lt>
2025-02-01 17:46:57 +0100

    Configure fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    alias ..2="cd ../.."
+    alias ..3="cd ../../.."
+    alias ..4="cd ../../../.."
+    alias ..5="cd ../../../../.."
+    alias ..6="cd ../../../../../.."
+    alias G=" \| grep "
+end
