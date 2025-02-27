af44bf95c86bb367ea70138c6c057c0aefad8fec
Thibault Polge <thibault@thb.lt>
2025-02-27 21:23:46 +0100

    [wip][fish] Configure prompt, untrack fish_variables

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,3 +6,9 @@ if status is-interactive
     alias ..6="cd ../../../../../.."
     alias G=" \| grep "
 end
+function fish_prompt
+    string join '' -- (set_color green) (prompt_pwd) (set_color normal) '>'
+end # interactive user name @ host name, date/time in YYYY-mm-dd format and path
+function fish_right_prompt
+    string join '' (fish_git_prompt)
+end # interactive user name @ host name, date/time in YYYY-mm-dd format and path
