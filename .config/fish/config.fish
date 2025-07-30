805fec697094f12af13c3090cc90bf7700466afe
Thibault Polge <thibault@thb.lt>
2025-07-30 17:54:06 +0200

    [fish] Some aliases for Emacs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,9 +6,30 @@ if status is-interactive
     alias ..6="cd ../../../../../.."
     alias G=" \| grep "
 end
+
+set __fish_git_prompt_showdirtystate true
+
+set __fish_git_prompt_showdirtystate yes
+set __fish_git_prompt_showstashstate yes
+set __fish_git_prompt_showuntrackedfiles yes
+set __fish_git_prompt_showupstream yes
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_untrackedfiles '☡'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '+'
+set __fish_git_prompt_char_upstream_behind -
 function fish_prompt
-    string join '' -- (set_color green) (prompt_pwd) (set_color normal) '>'
-end # interactive user name @ host name, date/time in YYYY-mm-dd format and path
+    string join '' -- (set_color green) (prompt_pwd) (set_color normal) ' > '
+end
+
 function fish_right_prompt
     string join '' (fish_git_prompt)
-end # interactive user name @ host name, date/time in YYYY-mm-dd format and path
+end
+
+alias e="emacsclient -n"
+alias magit="emacsclient -n -e \"(magit)\""
